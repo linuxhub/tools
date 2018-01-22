ada54976589133f08ede52a1078b4d357a2fd253
zepinglai <zepinglaiu@gmail.com>
2018-01-22 11:34:14 +0800

    【修改】脚本自动安装OMSA

--- ops/INSTALL.DELL.OMSA.sh
+++ ops/INSTALL.DELL.OMSA.sh
@@ -4,6 +4,9 @@
 #启动OMSA: /opt/dell/srvadmin/sbin/srvadmin-services.sh start
 #服务端口：https://localhost:1311
 
+#脚本自动安装 wget -q -O - https://raw.githubusercontent.com/linuxhub/tools/master/ops/INSTALL.DELL.OMSA.sh | bash
+
+
 yum install -y net-snmp net-snmp-devel net-snmp-utils wget perl OpenIPMI
 wget -q -O - http://linux.dell.com/repo/hardware/latest/bootstrap.cgi | bash
 yum  -y install srvadmin-all                        
