7731831354427fa5c1ebaa60f4fc66bb2297375a
泽泽 <root@linuxhub.org>
2018-05-09 12:03:05 +0800

    【更新】版本自动升级

--- open-falcon/falcon-agent/UPGRADE.sh
+++ open-falcon/falcon-agent/UPGRADE.sh
@@ -3,28 +3,35 @@
 # website:  https://www.linuxhub.org
 # Description: 更新open-falcon运维监控Agent客户端 upgrade
 
-# 自动执行： wget --no-check-certificate -q -O -https://raw.githubusercontent.com/linuxhub/tools/master/open-falcon/falcon-agent/UPGRADE.sh | bash
+# 自动执行： wget --no-check-certificate -q -O - https://raw.githubusercontent.com/linuxhub/tools/master/open-falcon/falcon-agent/UPGRADE.sh | bash
 
 if [ ! -f "/usr/local/falcon-agent/cfg.json" ];then
-	echo "No Falcon-Agent"
+    echo "没有检测到Falcon-Agent客户端配置文件,请先部署Falcon-Agent客户端。"
     exit
+fi
+
+version=`/usr/local/falcon-agent/falcon-agent -v`
+if [ "$version" == "5.1.1" ];then
+        echo "已经是最新版本"
+        exit
 fi
 
 wget --no-check-certificate -O /tmp/falcon-agent-5.1.1.tar.gz https://raw.githubusercontent.com/linuxhub/tools/master/open-falcon/falcon-agent/falcon-agent-5.1.1.tar.gz
 tar -zxf /tmp/falcon-agent-5.1.1.tar.gz -C /usr/local/
 
-mv /usr/local/falcon-agent /tmp/falcon-agent 
+mv /usr/local/falcon-agent /tmp/falcon-agent
 
 mv /usr/local/falcon-agent-5.1.1 /usr/local/falcon-agent
 cp /tmp/falcon-agent/cfg.json  /usr/local/falcon-agent/cfg.json
 
-if [ "$version" == "5.1.1" ];then
-	echo "版本升级成功: falcon-agent 5.1.1"
-	rm -rf /tmp/falcon-agent-5.1.1.tar.gz
-	rm -rf /tmp/falcon-agent
-else
-	echo "版本升级失败"
-	rm -rf /usr/local/falcon-agent
-	mv /tmp/falcon-agent /usr/local/falcon-agent
-fi
 
+version_new=`/usr/local/falcon-agent/falcon-agent -v`
+if [ "$version_new" == "5.1.1" ];then
+        echo "版本升级成功: falcon-agent 5.1.1"
+        rm -rf /tmp/falcon-agent-5.1.1.tar.gz
+        rm -rf /tmp/falcon-agent
+else
+        echo "版本升级失败"
+        rm -rf /usr/local/falcon-agent
+        mv /tmp/falcon-agent /usr/local/falcon-agent
+fi
