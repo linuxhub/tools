3f5d1392032fc4a900088de21481ea6cfbb77446
泽泽 <root@linuxhub.org>
2018-05-09 14:17:14 +0800

    【修改】脚本自动升级falcon-agent版本到5.1.1

--- open-falcon/falcon-agent/UPGRADE.sh
+++ open-falcon/falcon-agent/UPGRADE.sh
@@ -2,8 +2,11 @@
 # Author:   zeping lai
 # website:  https://www.linuxhub.org
 # Description: 更新open-falcon运维监控Agent客户端 upgrade
-
+#
+# Falcon-Agent版本数据库中查看: select * from falcon_portal.host where agent_version='5.1.1';
+#
 # 自动执行： wget --no-check-certificate -q -O - https://raw.githubusercontent.com/linuxhub/tools/master/open-falcon/falcon-agent/UPGRADE.sh | bash
+
 
 if [ ! -f "/usr/local/falcon-agent/cfg.json" ];then
     echo "没有检测到Falcon-Agent客户端配置文件,请先部署Falcon-Agent客户端。"
@@ -16,14 +19,17 @@ if [ "$version" == "5.1.1" ];then
         exit
 fi
 
+
 wget --no-check-certificate -O /tmp/falcon-agent-5.1.1.tar.gz https://raw.githubusercontent.com/linuxhub/tools/master/open-falcon/falcon-agent/falcon-agent-5.1.1.tar.gz
 tar -zxf /tmp/falcon-agent-5.1.1.tar.gz -C /usr/local/
 
 mv /usr/local/falcon-agent /tmp/falcon-agent
 
+
+/usr/local/falcon-agent/control stop
+
 mv /usr/local/falcon-agent-5.1.1 /usr/local/falcon-agent
 cp /tmp/falcon-agent/cfg.json  /usr/local/falcon-agent/cfg.json
-
 
 version_new=`/usr/local/falcon-agent/falcon-agent -v`
 if [ "$version_new" == "5.1.1" ];then
@@ -33,5 +39,7 @@ if [ "$version_new" == "5.1.1" ];then
 else
         echo "版本升级失败"
         rm -rf /usr/local/falcon-agent
-        mv /tmp/falcon-agent /usr/local/falcon-agent
+        mv /tmp/falcon-agent /usr/local/falcon-agent	
 fi
+
+/usr/local/falcon-agent/control start
