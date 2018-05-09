302f68913ade67edcfe479abc184b6bcadb868db
泽泽 <root@linuxhub.org>
2018-05-09 10:33:36 +0800

    【修改】MySQL数据库监控(适用于centos)

--- open-falcon/mymon/INSTALL.sh
+++ open-falcon/mymon/INSTALL.sh
@@ -3,20 +3,22 @@
 # website:  https://www.linuxhub.org
 # Description: open-falcon运维监控 mysql数据库监控
 
-# wget https://raw.githubusercontent.com/linuxhub/tools/master/open-falcon/mymon/INSTALL.sh
-# vim INSTALL.sh
-# bash INSTALL.sh
+# 脚本自动安装 wget -q -O - https://raw.githubusercontent.com/linuxhub/tools/master/open-falcon/mymon/INSTALL.sh  | bash
+
 
 if [ ! -f "/usr/local/falcon-agent/cfg.json" ];then
 	echo "No Falcon-Agent"
     exit
 fi
 
-wget -O /tmp/mymon.tar.gz https://raw.githubusercontent.com/linuxhub/tools/master/open-falcon/mymon/mymon.tar.gz
+wget --no-check-certificate -O /tmp/mymon.tar.gz https://raw.githubusercontent.com/linuxhub/tools/master/open-falcon/mymon/mymon.tar.gz
 tar -zxf /tmp/mymon.tar.gz -C /usr/local/
 rm -rf /tmp/mymon.tar.gz
 
 endpoint=`cat /usr/local/falcon-agent/cfg.json  | grep hostname | cut -d ":" -f2 |  sed 's/",*//g' | sed 's/^[ \t]*//g'`
-sed -i "s/endpoint=/endpoint=$endpoint" /usr/local/mymon/etc/mon.cfg
+sed -i "s/endpoint=/endpoint=$endpoint/g" /usr/local/mymon/etc/mon.cfg
 echo "*/1 * * * * cd /usr/local/mymon && ./mymon -c etc/mon.cfg" >> /var/spool/cron/root
 
+echo " -- mymon部署完成 --"
+echo "配置连接数据帐号密码"
+echo "配置文件: /usr/local/mymon/etc/mon.cfg"
